8769675eb8e9938c866b691000ebbbc620a86af6
Grisu118 <grisu118@grisu118.ch>
2015-04-02 14:45:05 +0200

    solved WS CountDownLatch

--- Lektion7/src/latch/Restaurant.java
+++ Lektion7/src/latch/Restaurant.java
@@ -1,22 +1,32 @@
 package latch;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Restaurant {
 	
 	public static void main(String[] args) {
 		int nrGuests = 2;
+
+		CountDownLatch l1 = new CountDownLatch(1);
+		CountDownLatch l2 = new CountDownLatch(nrGuests);
 				
-		new Cook(/*TODO */).start();
+		new Cook(l1).start();
 		
 		for(int i = 0; i < nrGuests; i++) {
-			new Guest(/*TODO */).start();
+			new Guest(l1, l2).start();
 		}
 		
-		new DishWasher(/*TODO */).start();
+		new DishWasher(l2).start();
 	}
 	
 	
 	static class Cook extends Thread {
-		public Cook(/*TODO */) {}
+
+		CountDownLatch l1;
+
+		public Cook(CountDownLatch l1) {
+			this.l1 = l1;
+		}
 		
 		@Override
 		public void run() {
@@ -25,37 +35,49 @@ public class Restaurant {
 				sleep(5000);
 			} catch (InterruptedException e) {}
 			System.out.println("Meal is ready");
-			/*TODO */
+			l1.countDown();
 		}
 	}
 	
 	
 	static class Guest extends Thread {
-		public Guest(/*TODO */) {}
+
+		CountDownLatch l1;
+		CountDownLatch l2;
+
+		public Guest(CountDownLatch l1, CountDownLatch l2) {
+			this.l1 = l1;
+			this.l2 = l2;
+		}
 		
 		@Override
 		public void run() {
 			try {
 				sleep(1000);
 				System.out.println("Entering restaurant and placing order.");
-				/*TODO */
+				l1.await();
 				System.out.println("Enjoying meal.");
 				sleep(5000);
 				System.out.println("Meal was excellent!");
-				/*TODO */
+				l2.countDown();
 			} catch (InterruptedException e) {}
 		}
 	}
 	
 	
 	static class DishWasher extends Thread {
-		public DishWasher(/*TODO */) {}
+
+		CountDownLatch l2;
+
+		public DishWasher(CountDownLatch l2) {
+			this.l2 = l2;
+		}
 		
 		@Override
 		public void run() {
 			try {
 				System.out.println("Waiting for dirty dishes.");
-				/*TODO */
+				l2.await();
 				System.out.println("Washing dishes.");
 				sleep(0);
 			} catch (InterruptedException e) {}
